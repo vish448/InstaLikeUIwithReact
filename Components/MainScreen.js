c9d467f6e126f7873223dd93526a029d45b10640
vishang soni <vishangsoni@gmail.com>
2018-08-28 23:53:32 -0400

    Adding navigation options and link fonts to xcdoe

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import {Icon} from 'native-base'
 
 
 class MainScreen extends Component {
+
+  static navigationOptions= {
+    headerLeft:<Icon name='ios-camera-outline' style={{paddingLeft: 10}}/>,
+    title: 'Instagram',
+    headerRight:<Icon name='ios-send-outline' style={{paddingRight:10}}/>,
+  }
+
   render() {
     return (
-
         <Text style ={styles.container}>MainScreen</Text>
-
     );
   }
 }
