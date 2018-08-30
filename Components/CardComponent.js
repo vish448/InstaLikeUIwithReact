e9532d8d019b5063dc8ae1a570cf5e4952b2e6c7
vishang soni <vishangsoni@gmail.com>
2018-08-29 23:51:51 -0400

    Adding a carditems to the Card

--- Components/CardComponent.js
+++ Components/CardComponent.js
@@ -5,6 +5,7 @@ import {
   View,
   Text,
   StyleSheet,
+  Image,
 } from 'react-native';
 
 import {Card, CardItem,Thumbnail,Body,Left,Right,Button,Icon} from 'native-base'
@@ -21,6 +22,37 @@ import {Card, CardItem,Thumbnail,Body,Left,Right,Button,Icon} from 'native-base'
               <Text note>July 4, 2018</Text>
             </Body>
           </Left>
+        </CardItem>
+        <CardItem cardBody>
+          <Image source={require('../assets/paint.jpg')} style={{height:200, width: null,flex:1}} />
+        </CardItem>
+        <CardItem style={{height:30}}>
+          <Left>
+            <Button transparent>
+              <Icon name='ios-heart-outline' style={{color: '#000'}} />
+            </Button>
+            <Button transparent>
+              <Icon name='ios-chatbubbles-outline' style={{color: '#000'}} />
+            </Button>
+            <Button transparent>
+              <Icon name='ios-send-outline' style={{color: '#000'}} />
+            </Button>
+          </Left>
+        </CardItem>
+        <CardItem style={{height:40}}>
+          <Text>93 Likes</Text>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text><Text style={{fontWeight: '800'}}>Vishag </Text>Some randome text comment.Some randome text comment.
+            Some randome text comment.
+            Some randome text comment
+            Some randome text comment
+            Some randome text comment
+            Some randome text comment
+            Some randome text comment
+            </Text>
+          </Body>
         </CardItem>
       </Card>
     );
