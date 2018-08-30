e15a587d97f005544c1c82711eaf20d09d416058
vishang soni <vishangsoni@gmail.com>
2018-08-30 00:21:12 -0400

    Addthing Stories thumbnails

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -5,11 +5,12 @@ import {
   View,
   Text,
   StyleSheet,
+  ScrollView,
 } from 'react-native';
 
 import CardComponent from '../CardComponent';
 
-import {Container, Content, Icon} from 'native-base';
+import {Container, Content, Icon, Thumbnail} from 'native-base';
 
 class HomeTab extends Component {
 
@@ -23,6 +24,30 @@ class HomeTab extends Component {
     return (
       <Container style={styles.container}>
         <Content>
+          <View style={{height:100}}>
+            <View style={{flex:1,justifyContent: 'space-between',flexDirection: 'row',alignItems: 'center',paddingHorizontal: 7}}>
+              <Text>Stories</Text>
+              <Text>Watch All</Text>
+            </View>
+            <View style={{flex:3}}>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalIndicator={false}
+                contentContainerStyle={{
+                  alignItems:'center',
+                  paddingStart:5,
+                  paddingEnd:5
+                }}>
+                <Thumbnail source={require('../../assets/me.jpg')} style = {{marginHorizontal:5,borderColor:'pink',borderWidth:2}}/>
+                <Thumbnail source={require('../../assets/me.jpg')} style = {{marginHorizontal:5,borderColor:'pink',borderWidth:2}}/>
+                <Thumbnail source={require('../../assets/me.jpg')} style = {{marginHorizontal:5,borderColor:'pink',borderWidth:2}}/>
+                <Thumbnail source={require('../../assets/me.jpg')} style = {{marginHorizontal:5,borderColor:'pink',borderWidth:2}}/>
+                <Thumbnail source={require('../../assets/me.jpg')} style = {{marginHorizontal:5,borderColor:'pink',borderWidth:2}}/>
+                <Thumbnail source={require('../../assets/me.jpg')} style = {{marginHorizontal:5,borderColor:'pink',borderWidth:2}}/>
+
+              </ScrollView>
+            </View>
+          </View>
           <CardComponent imageSource = 'paint' likes='30'/>
           <CardComponent imageSource = 'rafa1' likes='4240'/>
           <CardComponent imageSource = 'rafa2' likes='3450'/>
