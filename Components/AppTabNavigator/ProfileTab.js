b02d1f8e5e90371e348887c6356b29c7c84a8cea
vishang soni <vishangsoni@gmail.com>
2018-09-01 15:28:31 -0400

    Addding images to the first button view on the profile page

--- Components/AppTabNavigator/ProfileTab.js
+++ Components/AppTabNavigator/ProfileTab.js
@@ -6,11 +6,39 @@ import {
   Text,
   StyleSheet,
   Image,
+  Dimensions
 } from 'react-native';
 
 import {Icon, Container, Content, Header, Body, Left, Right, Button} from 'native-base';
 
 import EntypoIcon from 'react-native-vector-icons/Entypo'
+
+
+var images = [
+  require('../../assets/feedImages/feedImage1.jpg'),
+  require('../../assets/feedImages/feedImage2.jpg'),
+  require('../../assets/feedImages/feedImage3.jpg'),
+  require('../../assets/feedImages/feedImage4.jpg'),
+  require('../../assets/feedImages/feedImage5.jpg'),
+  require('../../assets/feedImages/feedImage6.jpg'),
+  require('../../assets/feedImages/feedImage7.jpg'),
+  require('../../assets/feedImages/feedImage8.jpg'),
+  require('../../assets/feedImages/feedImage9.jpg'),
+  require('../../assets/feedImages/feedImage10.jpg'),
+  require('../../assets/feedImages/feedImage11.jpg'),
+  require('../../assets/feedImages/feedImage12.jpg'),
+  require('../../assets/feedImages/feedImage13.jpg'),
+  require('../../assets/feedImages/feedImage14.jpg'),
+  require('../../assets/feedImages/feedImage15.jpg'),
+  require('../../assets/feedImages/feedImage16.jpg'),
+  require('../../assets/feedImages/feedImage17.jpg'),
+  require('../../assets/feedImages/feedImage18.jpg'),
+  require('../../assets/feedImages/feedImage19.jpg'),
+  require('../../assets/feedImages/feedImage20.jpg'),
+  require('../../assets/feedImages/feedImage21.jpg')
+]
+
+var {width,height} = Dimensions.get('window')
 
 class ProfileTab extends Component {
 
@@ -19,6 +47,41 @@ class ProfileTab extends Component {
       <Icon name='person' style={{color:tintColor}} />
     )
   }
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      activeIndex: 0
+    }
+
+  }
+
+  segmentClicked = (index) =>{
+    this.setState({
+      activeIndex: index
+    })
+  }
+
+  renderSectionOne = () => {
+    return images.map((image, index) => {
+      return(
+        <View key={index} style={[ {width:(width)/3}, {height:(height)/3}]} >
+          <Image source={image} style={{flex:1, width: undefined, height:undefined}} />
+        </View>
+      )
+    })
+  }
+
+  renderSection = () => {
+    if(this.state.activeIndex  == 0){
+      return(
+      <View style={{flexWrap: 'wrap',flexDirection: 'row'}}>
+        {this.renderSectionOne()}
+      </View>
+    )
+  }
+}
 
   render() {
     return (
@@ -64,6 +127,36 @@ class ProfileTab extends Component {
             <Text>Front End Developer</Text>
             <Text>www.vishangsoni.com</Text>
           </View>
+        </View>
+
+        <View>
+          <View style={{flexDirection: 'row',justifyContent: 'space-around',borderTopWidth: 1, borderTopColor: '#EEE'}}>
+            <Button transparent dark style={{color: '#CCC'}}
+                    onpress={()=>this.segmentClicked(0)}
+                    active={this.state.activeIndex == 0}>
+              <Icon name='ios-apps-outline'
+                    style={[this.state.activeIndex == 0 ? {} : {color:'grey'}]}></Icon>
+            </Button>
+            <Button transparent dark style={{color: '#CCC'}}
+                    onpress={()=>this.segmentClicked(1)}
+                    active={this.state.activeIndex == 1}>
+              <Icon name='ios-list-outline'
+                    style={[this.state.activeIndex == 1 ? {} : {color:'grey'}]}></Icon>
+            </Button>
+            <Button transparent dark style={{color: '#CCC'}}
+                    onpress={()=>this.segmentClicked(2)}
+                    active={this.state.activeIndex == 2}>
+              <Icon name='ios-people-outline'
+                    style={[this.state.activeIndex == 2 ? {} : {color:'grey'}]}></Icon>
+            </Button>
+            <Button transparent dark style={{color: '#CCC'}}
+                    onpress={()=>this.segmentClicked(3)}
+                    active={this.state.activeIndex == 3}>
+              <Icon name='ios-bookmark-outline'
+                    style={[this.state.activeIndex == 3 ? {} : {color:'grey'}]}></Icon>
+            </Button>
+          </View>
+          {this.renderSection()}
         </View>
       </Content>
     </Container>
